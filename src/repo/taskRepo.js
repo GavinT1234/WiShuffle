5bee3677454aaccd609e1f1fdffcdc5abde96e24
Gavin Turk <Gturk1@charlotte.edu>
2026-03-06 11:12:54 -0500

    Update after removing rules

---
+++ src/repo/taskRepo.js
@@ -0,0 +1,18 @@
+import prisma from '../config/db.js';
+
+export async function findAll() {
+  return prisma.task.findMany();
+}
+
+// Create a new task
+export async function create(data) {
+  return prisma.task.create({
+    data,
+  });
+}
+
+export async function findById(id) {
+  return prisma.task.findUnique({
+    where: { id },
+  });
+}
---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import * as taskController from '../controllers/taskController.js';
+import { validateTask } from '../middleware/validateTask.js';
+
+const router = express.Router();
+
+router.get('/', taskController.getTasks);
+router.post('/', validateTask, taskController.createTask);
+
+router.get('/:id', taskController.getTaskById);
+
+export default router;
+
